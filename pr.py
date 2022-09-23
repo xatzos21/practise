3a4b77ffa63441d807afab75c95804c42de63222
xatzos21 <xatzos21@gmail.com>
2022-09-23 14:29:20 +0200

    cr

--- pr.py
+++ pr.py
@@ -1,5 +1,19 @@
-def aek():
-    print("AEK r21")
+# def aek():
+#     print("AEK r21")
 
-def AEK():
-    print("asdf")
+# def AEK():
+#     print("asdf")
+
+names = ["Victor", "Peter", "Mary", "John", "Badara", "Peer"]
+print(names)
+names[5] = "Malcom X"
+print(names)
+names = names.extend("Martin")
+print(names)
+# 
+# using an index, replace "Peer" with "Malcom X"
+
+# Add two names to the list of names
+
+# Experiment with the methods in a list using dir(names)
+
