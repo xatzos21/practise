192c8dc988001c17074ec142ea47f96924b93b27
xatzos21 <xatzos21@gmail.com>
2022-09-23 14:40:06 +0200

    ö

--- pr.py
+++ pr.py
@@ -1,19 +1,22 @@
-# def aek():
-#     print("AEK r21")
-
-# def AEK():
-#     print("asdf")
-
-names = ["Victor", "Peter", "Mary", "John", "Badara", "Peer"]
-print(names)
-names[5] = "Malcom X"
-print(names)
-names = names.extend("Martin")
-print(names)
-# 
-# using an index, replace "Peer" with "Malcom X"
+# Task is by using an index
+# to replace "Peer" with "Malcom X"
 
 # Add two names to the list of names
 
 # Experiment with the methods in a list using dir(names)
 
+names = ["Victor", "Peter", "Mary", "John", "Badara", "Peer"]
+print(names)
+names[5] = "Malcom X"
+print(names)
+names.append("Martin")
+print(names)
+names.insert(1, "Patty")
+print(names)
+
+
+numbers = [2, 4, 6, 8, 10]
+print(numbers)
+numbers[1] = int(input("Choose new number: "))
+numbers[4] = int(input("Choose another number: "))
+print(numbers)
